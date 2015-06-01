05a9b28fead4871c9a5ee6b2dc0d7fe727f96232
ehjay <ehjay2015@gmail.com>
2015-06-01 14:48:30 -0400

    alias svn update commands for windows cmd

--- init.cmd
+++ init.cmd
@@ -1,1 +1,5 @@
 doskey ~=cd C:\Users\ajohnston
+doskey fup = cd C:\Users\ajohnston\java\foundation-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\foundation-www ^& cd , ^& svn up
+doskey stup = cd C:\Users\ajohnston\java\stratascape-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\stratascape-www ^& cd , ^& svn up
+doskey saup = cd C:\Users\ajohnston\java\salesscape-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\salesscape-www ^& cd , ^& svn up
+doskey bup = cd C:\Users\ajohnston\java\bankchoice-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\bankchoice-www ^& cd , ^& svn up
