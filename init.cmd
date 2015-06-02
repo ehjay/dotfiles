143ddfb06ba393488e6b9d03b8ec917b95d6ffc1
ehjay <ehjay2015@gmail.com>
2015-06-02 09:36:57 -0400

    cmd shortcuts to local directories

--- init.cmd
+++ init.cmd
@@ -1,4 +1,17 @@
-doskey ~=cd C:\Users\ajohnston
+doskey ~ = cd C:\Users\ajohnston
+
+doskey fapi = cd C:\Users\ajohnston\java\foundation-api
+doskey stapi = cd C:\Users\ajohnston\java\stratascape-api
+doskey saapi = cd C:\Users\ajohnston\java\salesscape-api
+doskey bapi = cd C:\Users\ajohnston\java\bankchoice-api
+
+doskey fwww = cd C:\Users\ajohnston\java\foundation-www
+doskey stwww = cd C:\Users\ajohnston\java\stratascape-www
+doskey sawww = cd C:\Users\ajohnston\java\salesscape-www
+doskey bwww = cd C:\Users\ajohnston\java\bankchoice-www
+
+doskey dot = cd C:\Users\ajohnston\java\dotfiles
+
 doskey fup = cd C:\Users\ajohnston\java\foundation-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\foundation-www ^& cd , ^& svn up
 doskey stup = cd C:\Users\ajohnston\java\stratascape-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\stratascape-www ^& cd , ^& svn up
 doskey saup = cd C:\Users\ajohnston\java\salesscape-api ^& cd , ^& svn up ^& cd C:\Users\ajohnston\java\salesscape-www ^& cd , ^& svn up
