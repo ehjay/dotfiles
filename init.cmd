691fa5d71951ba4cd54c655188328755236c7e9f
ehjay <ehjay@users.noreply.github.com>
2015-11-24 10:19:41 -0500

    add metricscape app to init

--- init.cmd
+++ init.cmd
@@ -14,6 +14,8 @@ set B_WWW=%JAVA%\bankchoice-www
 set P_API=%JAVA%\ptekintegration-api
 set P_WWW=%JAVA%\ptekintegration-www
 set BIG=%JAVA%\bigdata
+set M_API=%JAVA%\metricscape-api
+set M_WWW=%JAVA%\metricscape-www
 
 set DESKTOP=%HOME%\Desktop
 set DIFF_DIR=%DESKTOP%\diff
@@ -28,6 +30,8 @@ set B_WWW_DIFF=%DIFF_DIR%\bank-www.diff
 set P_API_DIFF=%DIFF_DIR%\ptek-api.diff
 set P_WWW_DIFF=%DIFF_DIR%\ptek-www.diff
 set BIG_DIFF=%DIFF_DIR%\bigdata.diff
+set M_API_DIFF=%DIFF_DIR%\metricscape-api.diff
+set M_WWW_DIFF=%DIFF_DIR%\metricscape-www.diff
 
 :: cd
 doskey ~ = cd %HOME%
@@ -43,39 +47,22 @@ doskey bwww = cd %B_WWW%
 doskey papi = cd %P_API%
 doskey pwww = cd %P_WWW%
 doskey big = cd %BIG%
+doskey mapi = cd %M_API%
+doskey mwww = cd %M_WWW%
 
 :: search subdirectories
 doskey s = findstr /s /i /c:"$*" *.*
 
-:: svn status
-doskey fstat = echo. ^& echo foundation-api status:^& echo. ^& svn st %F_API% ^& echo. ^& echo foundation-www status:^& echo. ^& svn st %F_WWW%
-doskey ststat = echo. ^& echo stratascape-api status:^& echo. ^& svn st %ST_API% ^& echo. ^& echo stratascape-www status:^& echo. ^& svn st %ST_WWW%
-doskey sastat = echo. ^& echo salesscape-api status:^& echo. ^& svn st %SA_API% ^& echo. ^& echo salesscape-www status:^& echo. ^& svn st %SA_WWW%
-doskey bstat = echo. ^& echo bankchoice-api status:^& echo. ^& svn st %B_API% ^& echo. ^& echo bankchoice-www status:^& echo. ^& svn st %B_WWW%
-doskey pstat = echo. ^& echo ptekintegration-api status:^& echo. ^& svn st %P_API% ^& echo. ^& echo ptekintegration-www status:^& echo. ^& svn st %P_WWW%
-doskey allstat = echo. ^& echo foundation-api status:^& echo. ^& svn st %F_API% ^& echo. ^& echo foundation-www status:^& echo. ^& svn st %F_WWW% ^& echo. ^& echo stratascape-api status:^& echo. ^& svn st %ST_API% ^& echo. ^& echo stratascape-www status:^& echo. ^& svn st %ST_WWW% ^& echo. ^& echo salesscape-api status:^& echo. ^& svn st %SA_API% ^& echo. ^& echo salesscape-www status:^& echo. ^& svn st %SA_WWW% ^& echo. ^& echo bankchoice-api status:^& echo. ^& svn st %B_API% ^& echo. ^& echo bankchoice-www status:^& echo. ^& svn st %B_WWW%^& echo ptekintegration-api status:^& echo. ^& svn st %P_API% ^& echo. ^& echo ptekintegration-www status:^& echo. ^& svn st %P_WWW%
-
 :: svn update
-doskey fup = svn up %F_API% ^& svn up %F_WWW%
-doskey stup = svn up %ST_API% ^& svn up %ST_WWW%
-doskey saup = svn up %SA_API% ^& svn up %SA_WWW%
-doskey bup = svn up %B_API% ^& svn up %B_WWW%
-doskey pup = svn up %P_API% ^& svn up %P_WWW%
-doskey bigup = svn up %BIG%
-doskey allup = svn up %F_API% ^& svn up %F_WWW% ^& svn up %ST_API% ^& svn up %ST_WWW% ^& svn up %SA_API% ^& svn up %SA_WWW% ^& svn up %B_API% ^& svn up %B_WWW% ^& svn up %P_API% ^& svn up %P_WWW% ^& svn up %BIG%
+doskey allup = svn up %F_API% ^& svn up %F_WWW% ^& svn up %ST_API% ^& svn up %ST_WWW% ^& svn up %SA_API% ^& svn up %SA_WWW% ^& svn up %B_API% ^& svn up %B_WWW% ^& svn up %P_API% ^& svn up %P_WWW% ^& svn up %BIG% ^& svn up %M_API% ^& svn up %M_WWW%
 
 :: svn diff
 :: output "directory already exists" error to unused var NULL
-doskey fdiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %F_API% $g %F_API_DIFF% ^& echo created %F_API_DIFF% ^& svn di %F_WWW% $g %F_WWW_DIFF% ^& echo created %F_WWW_DIFF%
-doskey stdiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %ST_API% $g %ST_API_DIFF% ^& echo created %ST_API_DIFF% ^& svn di %ST_WWW% $g %ST_WWW_DIFF% ^& echo created %ST_WWW_DIFF%
-doskey sadiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %SA_API% $g %SA_API_DIFF% ^& echo created %SA_API_DIFF% ^& svn di %SA_WWW% $g %SA_WWW_DIFF% ^& echo created %SA_WWW_DIFF%
-doskey bdiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %B_API% $g %B_API_DIFF% ^& echo created %B_API_DIFF% ^& svn di %B_WWW% $g %B_WWW_DIFF% ^& echo created %B_WWW_DIFF%
-doskey pdiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %P_API% $g %P_API_DIFF% ^& echo created %P_API_DIFF% ^& svn di %P_WWW% $g %P_WWW_DIFF% ^& echo created %P_WWW_DIFF%
-doskey alldiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %F_API% $g %F_API_DIFF% ^& echo created %F_API_DIFF% ^& svn di %F_WWW% $g %F_WWW_DIFF% ^& echo created %F_WWW_DIFF% ^& svn di %ST_API% $g %ST_API_DIFF% ^& echo created %ST_API_DIFF% ^& svn di %ST_WWW% $g %ST_WWW_DIFF% ^& echo created %ST_WWW_DIFF% ^& svn di %SA_API% $g %SA_API_DIFF% ^& echo created %SA_API_DIFF% ^& svn di %SA_WWW% $g %SA_WWW_DIFF% ^& echo created %SA_WWW_DIFF% ^& svn di %B_API% $g %B_API_DIFF% ^& echo created %B_API_DIFF% ^& svn di %B_WWW% $g %B_WWW_DIFF% ^& echo created %B_WWW_DIFF% ^& mkdir %DIFF_DIR% 2$g NULL ^& svn di %P_API% $g %P_API_DIFF% ^& echo created %P_API_DIFF% ^& svn di %P_WWW% $g %P_WWW_DIFF% ^& echo created %P_WWW_DIFF% ^& svn di %BIG% $g %BIG_DIFF% ^& echo created %BIG_DIFF%
-
+doskey alldiff = mkdir %DIFF_DIR% 2$g NULL ^& svn di %F_API% $g %F_API_DIFF% ^& echo created %F_API_DIFF% ^& svn di %F_WWW% $g %F_WWW_DIFF% ^& echo created %F_WWW_DIFF% ^& svn di %ST_API% $g %ST_API_DIFF% ^& echo created %ST_API_DIFF% ^& svn di %ST_WWW% $g %ST_WWW_DIFF% ^& echo created %ST_WWW_DIFF% ^& svn di %SA_API% $g %SA_API_DIFF% ^& echo created %SA_API_DIFF% ^& svn di %SA_WWW% $g %SA_WWW_DIFF% ^& echo created %SA_WWW_DIFF% ^& svn di %B_API% $g %B_API_DIFF% ^& echo created %B_API_DIFF% ^& svn di %B_WWW% $g %B_WWW_DIFF% ^& echo created %B_WWW_DIFF% ^& mkdir %DIFF_DIR% 2$g NULL ^& svn di %P_API% $g %P_API_DIFF% ^& echo created %P_API_DIFF% ^& svn di %P_WWW% $g %P_WWW_DIFF% ^& echo created %P_WWW_DIFF% ^& svn di %BIG% $g %BIG_DIFF% ^& echo created %BIG_DIFF% ^& svn di %M_API% $g %M_API_DIFF% ^& echo created %M_API_DIFF% ^& svn di %M_WWW% $g %M_WWW_DIFF% ^& echo created %M_WWW_DIFF%
 
 :: run www server
 doskey strun = cd %ST_WWW% ^& grunt server
 doskey sarun = cd %SA_WWW% ^& grunt server
 doskey brun = cd %B_WWW% ^& grunt server
 doskey prun = cd %P_WWW% ^& grunt server
+doskey mrun = cd %M_WWW% ^& grunt server
